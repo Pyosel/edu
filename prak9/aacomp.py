6ca40c3e8de28e96acd66a72f8407b0656aaf6b1
Natalia Kondratenko <nataliadkon@gmail.com>
2017-11-15 22:34:41 +0300

    Init

--- prak9/aacomp.py
+++ prak9/aacomp.py
@@ -1,14 +1,27 @@
 from sys import argv
-indata = open(argv[1])
+if len(argv) == 3:
+    indata = open(argv[1])
+    outf = open(argv[2], 'w')
+if len(argv) == 2:
+    indata = open(argv[1])
+if len(argv) == 1:
+    print 'Enter file name:'
+    indata = open(raw_input())
 indata = indata.read().splitlines()
-outf = open(argv[2], 'w')
-d = {}
-x = 1
-for str in indata:
-    for i in str[x]:
+dict = {}
+for x in range(1,len(indata)):
+    for i in indata[x]:
+        i = i.upper()
         res = dict.get(i)
         if res == None:
             dict[i] = 1
         else:
             dict[i] = res+1
-print dict
+for key in sorted(dict):
+    res = dict.get(key)
+    if len(argv) == 3:
+        key = str(key)
+        res = str(res)
+        outf.write(key + '\t' + res + '\n' )
+    if len(argv) == 2 or len(argv) == 1:
+        print key, '\t', res
--- prak9/randomdna.py
+++ prak9/randomdna.py
@@ -1,6 +1,10 @@
 nucleobase = 'ATGC'
 from random import randint
 from sys import argv
+split = len(argv) == 3
+splitInterval = 0
+if split:
+    splitInterval = int(argv[2])
 outf = open('randomseq.fasta', 'w')
 outf.write('>random\n')
 seqlength = int(argv[1])
@@ -8,4 +12,7 @@ for i in range(0, seqlength):
     a = randint(0,3)
     res = nucleobase[a]
     outf.write(res)
+    if split and (i + 1) % splitInterval == 0:
+        outf.write("\n")
 outf.close()
+indata.close()
