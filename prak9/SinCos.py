ba421cd60bafe68e8da27bef6f838a3dc0dc1ecc
Natalia Kondratenko <nataliadkon@gmail.com>
2017-11-16 22:19:00 +0300

    Init

--- prak9/SinCos.py
+++
@@ -1,7 +0,0 @@
-import math
-from sys import argv
-degreeInput = float(argv[1])
-radiansInput = math.radians(degreeInput)
-
-print round(math.sin(radiansInput), 4)
-print round(math.cos(radiansInput), 4)
